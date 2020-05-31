b10fdbc96445cbffa6f85b4605efde659b8501ef
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-31 11:12:34 -0500

    Defining the function 'deploy menu'

---
+++ Functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void deployMenu();	//declaring the function
+
+int main(){
+	deployMenu();
+
+
+	return 0;
+}
+
+void deployMenu(){	//defining the function
+	cout<<"1. Save Information"<<endl;
+	cout<<"2, Delete Information"<<endl;
+	cout<<"3. Edit Information"<<endl;
+	cout<<"4. QUIT"<<endl;
+}
